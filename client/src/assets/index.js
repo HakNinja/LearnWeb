d269fe1587312131ddfbe60562a5d986f0a980de
Anshul Pant <anshulpant14@gmail.com>
2024-03-07 11:13:07 +0530

    pincode functionality added

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,4 +1,4 @@
-import {default as Logo} from './logo.png';
+import {default as Logo} from './Logo.png';
 import {default as about1} from './about1.jpeg'
 import {default as avatar} from './avatar.jpeg'
 import {default as banner} from './banner.png'
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { HiMenuAlt4, HiX } from 'react-icons/hi';
 import { motion } from 'framer-motion';
 import './Navbar.scss'
-import { LWLogo } from '../../assets';
+import { Logo } from '../../assets';
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom'
 import { Link } from "react-router-dom"
@@ -19,7 +19,7 @@ const Navbar = () => {
   return (
     <nav className='navbar colorGradient'>
       <div className='navbar-logo'>
-        <Link to="/"><img src={LWLogo} alt="logo" className='opacity-90' /></Link>
+        <Link to="/"><img src={Logo} alt="logo" className='opacity-90' /></Link>
       </div>
       <ul className='navbar-links'>
         {['Home', 'Courses', 'Roadmap', 'About us'].map((items) =>
