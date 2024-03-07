756dfc0cc5257c7c8bee3e4cf7d06ffbba697c28
Anshul Pant <anshulpant14@gmail.com>
2024-03-07 07:18:14 +0530

    pincode functionality added

--- Server/controllers/data.js
+++ Server/controllers/data.js
@@ -3,16 +3,27 @@
  
  export const pincodeDataAutomation = async (req, res) => {
     try {
-     console.log(pinData)
-     const A = req.body.pincode
-    //  const B = await User.create(A)
+    //  console.log(pinData)
+    //  const A = req.query.pincode
+     console.log(A)
     const B = pinData.filter((pin)=>{
        return pin.pincode==A
     })
-    console.log(B)
-    //  await B.save()
-      res.status(200).json({ "success": B[0] });
+    // console.log(Object.keys(B[0]).length)
+    if(Object.keys(B[0]).length < 5){
+      res.status(200).json({message:"Pincode not found try again with different pincode"})
+    }else{
+     const C = B[0]
+     C.Success=true
+          
+      res.status(200).json({ "Response": C });
+
+    }
+   
     } catch (error) {
       res.status(404).json({ message: error.message });
     }
   }
+
+      //     .then((response) => response.json())
+        //     .then((data) => console.log(data.Response.Success))
--- client/src/components/PersonData.jsx
+++ client/src/components/PersonData.jsx
@@ -8,32 +8,61 @@ const PersonData = () => {
     const [phone, setPhone] = useState('')
     const [pincode, setPincode] = useState('')
     const [address, setAddress] = useState('')
-    const [password, setPassword] = useState('')
-    const [cpassword, setCpassword] = useState('')
-    const [npassword, setNpassword] = useState('')
+    const [city, setCity] = useState('')
+    const [district, setDistrict] = useState('')
+    const [stateName, setStateName] = useState('')
+    const [instituteName, setInstituteName] = useState('')
 
     const handleChange = async (e) => {
 
-        if (e.target.name == 'name') {
+        if (e.target.name === 'name') {
             setName(e.target.value)
         }
-        else if (e.target.name == 'phone') {
+        else if (e.target.name === 'phone') {
             setPhone(e.target.value)
         }
-        else if (e.target.name == 'pincode') {
+        else if (e.target.name === 'pincode') {
             setPincode(e.target.value)
+            if (e.target.value.length === 6) {
+                getpincode(e.target.value)
+            }
+            else {
+                setCity('')
+                setStateName('')
+            }
         }
-        else if (e.target.name == 'address') {
+        else if (e.target.name === 'address') {
             setAddress(e.target.value)
         }
-        else if (e.target.name == 'password') {
-            setPassword(e.target.value)
+        else if (e.target.name === 'city') {
+            setCity(e.target.value)
         }
-        else if (e.target.name == 'cpassword') {
-            setCpassword(e.target.value)
+        else if (e.target.name === 'district') {
+            setDistrict(e.target.value)
         }
-        else if (e.target.name == 'npassword') {
-            setNpassword(e.target.value)
+        else if (e.target.name === 'stateName') {
+            setStateName(e.target.value)
+        }
+        else if (e.target.name === 'instituteName') {
+            setInstituteName(e.target.value)
+        }
+    }
+
+    const getpincode = async (pin) => {
+        let data = pin
+        let pins = await fetch(`http://127.0.0.1:5001/data/pinData?pincode=${data}`)
+        let response = await pins.json()
+        console.log(response.Response.Success)
+        if (response.Response.Success) {
+            setCity(response.Response.taluk)
+            setDistrict(response.Response.districtName)
+            setStateName(response.Response.stateName)
+
+        }
+        else {
+            setCity('')
+            setDistrict('')
+            setStateName('')
         }
     }
 
@@ -56,8 +85,8 @@ const PersonData = () => {
                 pauseOnHover
                 theme="light"
             />
-            <h1 className='text-2xl text-center font-bold'>Update your Account</h1>
-            <h2 className='text-xl mx-auto font-semibold mb-4'>1. Customer Details.</h2>
+            <h1 className='text-2xl text-center font-bold'>Learner Biodata</h1>
+            {/* <h2 className='text-xl mx-auto font-semibold mb-4'> Customer Details.</h2> */}
             <div className="mx-auto flex">
                 <div className="px-2 w-1/2">
                     <label htmlFor="name"
@@ -80,7 +109,7 @@ const PersonData = () => {
             </div>
             <div className="px-2 w-full">
                 <label htmlFor="address"
-                    className="leading-7 text-sm text-gray-600">Address</label>
+                    className="leading-7 text-sm text-gray-600">Street and House Number</label>
                 <textarea id="address"
                     name="address"
                     cols='30' rows='2'
@@ -92,7 +121,7 @@ const PersonData = () => {
                 <div className="px-2 w-1/2">
                     <label htmlFor="phone"
                         className="leading-7 text-sm text-gray-600">Phone</label>
-                    <input type="text"
+                    <input type="tel"
                         id="phone"
                         name="phone"
                         placeholder='10 Digit - Phone Number'
@@ -111,7 +140,51 @@ const PersonData = () => {
                         className="w-full bg-white rounded border border-gray-300 focus:border-pink-500 focus:ring-2 focus:ring-pink-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                 </div>
             </div>
-            <button onClick={handleUserSubmit} className='m-2 flex ml-2 mt-4 p-8 text-white bg-pink-500 border-0 py-2 focus:outline-none hover:bg-pink-700 rounded text-sm' >Update Details</button>
+            <div className="mx-auto flex">
+                <div className="px-2 w-1/2">
+                    <label htmlFor="city"
+                        className="leading-7 text-sm text-gray-600">City</label>
+                    <input type="text"
+                        id="city"
+                        name="city"
+                        onChange={handleChange}
+                        value={city}
+                        className="w-full bg-white rounded border border-gray-300 focus:border-pink-500 focus:ring-2 focus:ring-pink-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                </div>
+                <div className="px-2 w-1/2">
+                    <label htmlFor="district"
+                        className="leading-7 text-sm text-gray-600">District</label>
+                    <input type="text"
+                        id="district"
+                        name="district"
+                        onChange={handleChange}
+                        value={district}
+                        className="w-full bg-white rounded border border-gray-300 focus:border-pink-500 focus:ring-2 focus:ring-pink-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                </div>
+            </div>
+            <div className="mx-auto flex">
+                <div className="px-2 w-1/2">
+                    <label htmlFor="stateName"
+                        className="leading-7 text-sm text-gray-600">State</label>
+                    <input type="text"
+                        id="stateName"
+                        name="stateName"
+                        onChange={handleChange}
+                        value={stateName}
+                        className="w-full bg-white rounded border border-gray-300 focus:border-pink-500 focus:ring-2 focus:ring-pink-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                </div>
+                <div className="px-2 w-1/2">
+                    <label htmlFor="instituteName"
+                        className="leading-7 text-sm text-gray-600">Institute Name</label>
+                    <input type="text"
+                        id="instituteName"
+                        name="instituteName"
+                        onChange={handleChange}
+                        value={instituteName}
+                        className="w-full bg-white rounded border border-gray-300 focus:border-pink-500 focus:ring-2 focus:ring-pink-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                </div>
+            </div>
+            <button onClick={handleUserSubmit} className='m-2 flex ml-2 mt-4 p-8 text-white bg-pink-500 border-0 py-2 focus:outline-none hover:bg-pink-700 rounded text-sm' >Save Details</button>
 
 
             {/* <h2 className='text-xl mx-auto font-semibold my-8'>2. Update Password.</h2>
