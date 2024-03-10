3e1270c6efb51f7cc78f0d42760bd87ad8878cee
Deepak <deepakkumararya1812@gmail.com>
2024-03-11 00:21:04 +0530

    add courses

--- client/src/components/Courses/CourseList.jsx
+++ client/src/components/Courses/CourseList.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
 import courses from './courses.json';
 
 const CourseList = () => {
@@ -10,17 +12,21 @@ const CourseList = () => {
     setIsBoxVisible(true);
   };
 
-  const handleClose = () => {
-    setIsBoxVisible(false);
-  };
-
   return (
     <div>
-      <h1>Courses</h1>
-      <ul>
+      <h1>Courses Available : </h1>
+      <VerticalTimeline>
         {courses.courses.map((course) => (
-          <li key={course.id} onClick={() => handleClick(course)}>
-            {course.title}
+          <VerticalTimelineElement
+            key={course.id}
+            date={course.date} // Add the date of the course here
+            iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }} // Customize icon style if needed
+            // contentStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }} // Customize content style if needed
+            // icon={<Icon />} // You can add custom icons if needed
+          >
+            <h3 className="vertical-timeline-element-title" onClick={() => handleClick(course)}>
+              {course.title}
+            </h3>
             {selectedCourse === course && (
               <ul>
                 {course.steps.map((step) => (
@@ -28,9 +34,9 @@ const CourseList = () => {
                 ))}
               </ul>
             )}
-          </li>
+          </VerticalTimelineElement>
         ))}
-      </ul>
+      </VerticalTimeline>
     </div>
   );
 };
