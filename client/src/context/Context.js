2e23d8659780e4cce4e2ef1425be16e98d271c0e
Anshul Pant <anshulpant14@gmail.com>
2024-03-12 11:53:32 +0530

    logo added

--- client/src/context/Context.js
+++ client/src/context/Context.js
@@ -1,39 +1,38 @@
 import { useContext, useState, useEffect, createContext } from 'react';
-import { googleLogout } from '@react-oauth/google';
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
     const [role, setRole] = useState(1);
-    const [isGUser, setIsGUser] = useState(false);
+    // const [isGUser, setIsGUser] = useState(false);
     const [user, setUser] = useState('');
-    const logIn = (userData, isGUser, role) => {
+    const logIn = (userData, role) => {
         setRole(role);
         setUser(userData);
-        setIsGUser(isGUser);
+        // setIsGUser(isGUser);
         localStorage.setItem('role', role);
-        localStorage.setItem('isGUser', isGUser);
+        // localStorage.setItem('isGUser', isGUser);
         localStorage.setItem('userData', JSON.stringify(userData));
     }
-    const logOut = () => {
-        if(isGUser){
-            googleLogout();
-        }
-        // setIsAuth(false);
-        setIsGUser(false);
-        sessionStorage.clear();
-    }
+    // const logOut = () => {
+    //     if(isGUser){
+    //         googleLogout();
+    //     }
+    //     // setIsAuth(false);
+    //     setIsGUser(false);
+    //     sessionStorage.clear();
+    // }
     useEffect(() => {
-        const storedIsAuth = sessionStorage.getItem('isAuth');
-        const storedIsGUser = sessionStorage.getItem('isGUser');
+        const storedrole = sessionStorage.getItem('role');
+        // const storedIsGUser = sessionStorage.getItem('isGUser');
         const userData = JSON.parse(sessionStorage.getItem('userData'));
-        if (storedIsAuth === 'true') {
-            setIsGUser(storedIsGUser);
+        // if (storedIsAuth === 'true') {
+            // setIsGUser(storedIsGUser);
             // setIsAuth(true);
             setUser(userData);
-        }
+        // }
     }, [])
     return (
-        <AuthContext.Provider value={{ role, isGUser, user, logIn, logOut }}>
+        <AuthContext.Provider value={{ role,setRole, user, logIn}}>
             {children}
         </AuthContext.Provider>
     )
