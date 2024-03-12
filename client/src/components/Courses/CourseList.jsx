2f349e49ba2720d3e91947b94f6709d574ceae2b
Anshul Pant <anshulpant14@gmail.com>
2024-03-12 10:31:40 +0530

    roadmap

--- client/src/components/Courses/CourseList.jsx
+++ client/src/components/Courses/CourseList.jsx
@@ -19,35 +19,35 @@ function CourseList() {
   };
 
   return (
-    <div className="App">
+    <div className="App min-h-[90vh] relative">
       <h1>Free Courses</h1>
       {/* <div className='fixedme'> */}
-        <div className="course-container">
-          {coursesData.courses.map((course, index) => (
-            <div className="course" key={index} onClick={() => handleCourseClick(course)}>
-              <p>{course.course_name}</p>
-              <p>{course.description}</p>
-            </div>
-          ))}
-        </div>
-
-        {selectedCourse &&
-          <div className="course-roadmap" >
-            <h2>{selectedCourse.course_name} Roadmap</h2>
-            <button onClick={handleCloseClick}> Close </button>
-            <div class="timeline">
-              {selectedCourse.steps.map((step, stepIndex) => (
-                <div className={stepIndex % 2 === 0 ? "container left" : "container right"} key={stepIndex}>
-                  <div className="content">
-                    <h2>Chapter {stepIndex + 1}</h2>
-                    <p>{step}</p>
-                  </div>
-                </div>
-              ))}
-            </div>
+      <div className="course-container ">
+        {coursesData.courses.map((course, index) => (
+          <div className="course" key={index} onClick={() => handleCourseClick(course)}>
+            <p>{course.course_name}</p>
+            <p>{course.description}</p>
           </div>
-        }
+        ))}
       </div>
+
+      {selectedCourse &&
+        <div className="course-roadmap " >
+          <h2>{selectedCourse.course_name} Roadmap</h2>
+          <button onClick={handleCloseClick} className='absolute top-5 right-5  btn btn-light'> Close </button>
+          <div className="timeline">
+            {selectedCourse.steps.map((step, stepIndex) => (
+              <div className={stepIndex % 2 === 0 ? "container left" : "container right"} key={stepIndex}>
+                <div className="content">
+                  <h2>Chapter {stepIndex + 1}</h2>
+                  <p>{step}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      }
+    </div>
     // </div>
   )
 }
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,34 +3,34 @@ import React from 'react'
 const Footer = () => {
   return (
     < div style={{ display: "flex" }} className='colorGradient'>
-      <div class="col mb-3 text-center" style={{ marginTop: "3rem" }}>
-        <p class="text-body-secondary">© 2024 LearnWeb</p>
-        <i class="fa-brands fa-instagram"></i>
-        <i class="fa-brands fa-twitter"></i>
-        <i class="fa-brands fa-linkedin"></i>
-        <i class="fa-brands fa-youtube"></i>
+      <div className="col mb-3 text-center" style={{ marginTop: "3rem" }}>
+        <p className="text-body-secondary">© 2024 LearnWeb</p>
+        <i className="fa-brands fa-instagram"></i>
+        <i className="fa-brands fa-twitter"></i>
+        <i className="fa-brands fa-linkedin"></i>
+        <i className="fa-brands fa-youtube"></i>
       </div>
 
 
-      <div class="col mb-3 text-center" style={{ marginTop: "3rem" }}>
+      <div className="col mb-3 text-center" style={{ marginTop: "3rem" }}>
         <h5>Features</h5>
-        <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="/" class="nav-link p-0 text-body-secondary">LearWeb Business</a></li>
-          <li class="nav-item mb-2"><a href="/" class="nav-link p-0 text-body-secondary">Teach on LearnWed</a></li>
-          <li class="nav-item mb-2"><a href="/" class="nav-link p-0 text-body-secondary">About us</a></li>
-          <li class="nav-item mb-2"><a href="/" class="nav-link p-0 text-body-secondary">Contact us</a></li>
-          <li class="nav-item mb-2"><a href="/" class="nav-link p-0 text-body-secondary">Get the App</a></li>
+        <ul className="nav flex-column">
+          <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-body-secondary">LearWeb Business</a></li>
+          <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-body-secondary">Teach on LearnWed</a></li>
+          <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-body-secondary">About us</a></li>
+          <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-body-secondary">Contact us</a></li>
+          <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-body-secondary">Get the App</a></li>
         </ul>
       </div>
 
-      <div class="col mb-3 text-center" style={{ marginTop: "3rem" }}>
+      <div className="col mb-3 text-center" style={{ marginTop: "3rem" }}>
         <h5>Careers</h5>
-        <ul class="nav flex-column">
-          <li class="nav-item mb-2"><a href="/" class="nav-link p-0 text-body-secondary">Blog</a></li>
-          <li class="nav-item mb-2"><a href="/" class="nav-link p-0 text-body-secondary">Help and Support</a></li>
-          <li class="nav-item mb-2"><a href="/" class="nav-link p-0 text-body-secondary">Affiliate</a></li>
-          <li class="nav-item mb-2"><a href="/" class="nav-link p-0 text-body-secondary">Investors</a></li>
-          <li class="nav-item mb-2"><a href="/" class="nav-link p-0 text-body-secondary">Sitemap</a></li>
+        <ul className="nav flex-column">
+          <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-body-secondary">Blog</a></li>
+          <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-body-secondary">Help and Support</a></li>
+          <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-body-secondary">Affiliate</a></li>
+          <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-body-secondary">Investors</a></li>
+          <li className="nav-item mb-2"><a href="/" className="nav-link p-0 text-body-secondary">Sitemap</a></li>
         </ul>
       </div>
     </ div>
