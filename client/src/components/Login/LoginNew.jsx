3f27d574c315d31589a83bed861634fa6b783be7
Anshul Pant <anshulpant14@gmail.com>
2024-03-13 13:14:23 +0530

    adjust css

--- client/src/components/Login/LoginNew.jsx
+++ client/src/components/Login/LoginNew.jsx
@@ -8,40 +8,31 @@ const LoginNew = () => {
     const navigate = useNavigate()
     const handleInstructerLogin = () => {
         setRole(1);
-        navigate('/login')
+        navigate('/loginsignup')
     }
-    const handleInstructerSignup = () => {
-        setRole(1);
-        navigate('/signup')
-    }
+
     const handleStudentLogin = () => {
         setRole(2);
         console.log(role)
-        navigate('/login')
+        navigate('/loginsignup')
     }
-    const handleStudentSignup = () => {
-        setRole(2);
-        console.log(role)
-        navigate('/signup')
-    }
+
     return (
         <>
-            <div className="login-Container">
+            <div className="login-Container min-h-[45vh] ">
                 <div className="instructor-login">
                     <h2>For Instructor</h2>
                     <div className='test'>  <p>We are the market–leading technical interview platform to identify and hire developers with the right skills</p> </div>
 
-                    <button class=" btn btn-light login" onClick={handleInstructerLogin}>Login</button>
+                    <button class=" btn btn-light login" onClick={handleInstructerLogin}>Login Sign</button>
 
-                    <div className='test'>  <p>Don't have an account? <br />
-                        <button class=" btn btn-light signup" onClick={handleInstructerSignup}>SignUp</button></p> </div>
+
                 </div>
-                <div className="learner-login">
+                <div className="learner-login ">
                     <h2>For Learner</h2>
                     <div className='test'>  <p>Join over 21 million developers, practice coding skills, prepare for interviews, and get hired.</p> </div>
-                    <button class=" btn btn-light login" onClick={handleStudentLogin}>Login</button>
-                    <div className='test'>  <p>Don't have an account? <br />
-                        <button class=" btn btn-light signup" onClick={handleStudentSignup}>SignUp</button></p> </div>
+                    <button class=" btn btn-light login" onClick={handleStudentLogin}>Login Sign</button>
+
                 </div>
             </div>
         </>
