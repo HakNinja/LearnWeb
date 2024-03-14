a6a752ac23d9a6b215943892ad61413026bb6386
YogeshTiwari10 <yogeshtiwari733@gmail.com>
2024-03-14 15:59:49 +0530

    checkout page

--- client/src/pages/CheckOut/CheckOut.jsx
+++ client/src/pages/CheckOut/CheckOut.jsx
@@ -17,28 +17,42 @@ const CheckOut = () => {
         <div className='checkout'>
             <div className='c-left'>
                 <h2>Checkout</h2>
-                <table>
-                    <thead>
-                        <tr style={{display:"flex", justifyContent:"space-between"}}>
-                            <th>Course Name</th>
-                            <th>Description</th>
-                            <th>Price</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr style={{display:"flex", justifyContent:"space-between"}}>
-                            <td>
-                                {/* <div style={{display:"flex", flexDirection:"column", justifyContent:"flex-start"}}> */}
-                                    <img src={details.c_image} alt=""  style={{width:"200px", border:"2px solid blue"}}/>
-                                    <p> {details.course_name} </p>
-                                {/* </div> */}
-                            </td>
-                            <td>description</td>
-                            <td>price</td>
-                        </tr>
-                    </tbody>
-                </table>
-                <div>Discount</div>
+            <div class="relative overflow-x-auto">
+    <table class="w-full text-l text-left rtl:text-right text-black-500 dark:text-gray-400">
+        <thead class=" text-white text-lg uppercase bg-pink-600">
+            <tr>
+                <th scope="col" class="px-6 py-3 rounded-s-lg">
+                    Product name
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Instructor
+                </th>
+                <th scope="col" class="px-6 py-3 rounded-e-lg">
+                    Price
+                </th>
+            </tr>
+        </thead>
+        <tbody className='rounded'>
+            <tr class="bg-pink-100 text-gray-900 rounded-lg ">
+                <th scope="row" class="px-6 py-4 font-medium rounded-s-lg text-gray-900 whitespace-nowrap">
+                    <img src={details.c_image} alt="" style={{width:"200px", borderRadius:"15px"}} />
+                    <div className='text-l text-gray-900 font-bold mt-4'>{details.course_name}</div>
+                </th>
+                <td class="px-6 py-4 text-l text-gray-900 font-bold">
+                    {details.tutor_name}
+                </td>
+                <td class="px-6 py-4 text-l text-gray-900 font-bold">
+                ₹ {details.price}
+                </td>
+            </tr>
+            
+        </tbody>
+    </table>
+</div>
+                <div className='discount'>
+                   <div className='dis-text'> Got Your Coupons? Apply Here</div>
+                   <div><input type="text" placeholder='Coupon Code' className='input-field1' /> <button className='p-button1'>Apply</button></div>
+                </div>
             </div>
             <div className='c-right'>
                 right
